64afca36e8f46516a9bb1055be3bfc2cc3aaf57b
Rikuto <mikadorikuto@gmail.com>
2026-01-29 23:51:20 +0900

    update the code

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 
+// Import the root App component and child components
 import App from './App.vue';
 import TheHeader from './components/TheHeader.vue';
 import BaseBadge from './components/BaseBadge.vue';
@@ -8,6 +9,7 @@ import UserInfo from './components/UserInfo.vue';
 
 const app = createApp(App);
 
+// Register components globally so they can be used in any template
 app.component('the-header', TheHeader);
 app.component('base-badge', BaseBadge);
 app.component('badge-list', BadgeList);
