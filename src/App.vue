07d9a0e636576f01f53fd8f4fff8dd7d9246efd3
Rikuto <mikadorikuto@gmail.com>
2026-02-04 16:11:26 +0900

    udpate the code

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <the-header></the-header>
+    <TheHeader />
     <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
@@ -11,7 +11,19 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
 export default {
+  components: {
+    // 'the-header': TheHeader,
+    // TheHeader: TheHeader,
+    // ES6 shorthand: same as "TheHeader: TheHeader"
+    TheHeader,
+    BadgeList,
+    UserInfo,
+  },
   data() {
     return {
       activeUser: {
--- src/main.js
+++ src/main.js
@@ -2,17 +2,11 @@ import { createApp } from 'vue';
 
 // Import the root App component and child components
 import App from './App.vue';
-import TheHeader from './components/TheHeader.vue';
 import BaseBadge from './components/BaseBadge.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
 
 const app = createApp(App);
 
 // Register components globally so they can be used in any template
-app.component('the-header', TheHeader);
 app.component('base-badge', BaseBadge);
-app.component('badge-list', BadgeList);
-app.component('user-info', UserInfo);
 
 app.mount('#app');
